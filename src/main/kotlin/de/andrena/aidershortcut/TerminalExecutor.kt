7be7982ace59999f8f00ad2e414124df8151c572
Peter Wegner <peter.wegner@andrena.de>
2024-09-02 19:27:02 +0200

    feat: enhance TerminalExecutor with logging and additional imports

--- src/main/kotlin/de/andrena/aidershortcut/TerminalExecutor.kt
+++ src/main/kotlin/de/andrena/aidershortcut/TerminalExecutor.kt
@@ -1,12 +1,23 @@
 package de.andrena.aidershortcut
 
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.WriteAction
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import org.jetbrains.plugins.terminal.TerminalToolWindowManager
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.openapi.vfs.newvfs.RefreshQueue
 import java.awt.EventQueue.invokeLater
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import kotlin.concurrent.thread
 
-class TerminalExecutor(private val project: Project, private val commandData: CommandData, private val files: Array<VirtualFile>) {
+class TerminalExecutor(
+    private val project: Project,
+    private val commandData: CommandData,
+    private val files: Array<VirtualFile>
+) {
+    private val LOG = Logger.getInstance(TerminalExecutor::class.java)
     fun execute() {
         val progressDialog = ProgressDialog(project, "Aider Command in Progress")
         thread {
