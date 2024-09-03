8c84af11961413f5c1ba1ff8281762d06c01d2fd
Peter Wegner <peter.wegner@andrena.de>
2024-09-04 01:32:55 +0200

    refactor: format code for better readability in AiderAction.kt

--- src/main/kotlin/de/andrena/aidershortcut/AiderAction.kt
+++ src/main/kotlin/de/andrena/aidershortcut/AiderAction.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
 import de.andrena.aidershortcut.command.CommandData
 import de.andrena.aidershortcut.executors.IDEBasedExecutor
 import de.andrena.aidershortcut.executors.ShellExecutor
@@ -29,7 +30,12 @@ class AiderAction : AnAction() {
                 if (commandData.isShellMode) {
                     ShellExecutor(project, commandData).execute()
                 } else {
-                    IDEBasedExecutor(project, commandData, dialog.getWriteableFiles().map { VirtualFileManager.getInstance().findFileByUrl(it) }.toTypedArray()).execute() // Pass writeableFiles as VirtualFile array
+                    IDEBasedExecutor(
+                        project,
+                        commandData,
+                        dialog.getWriteableFiles().map { VirtualFileManager.getInstance().findFileByUrl(it) }
+                            .toTypedArray()
+                    ).execute() // Pass writeableFiles as VirtualFile array
                 }
                 dialog.addToHistory(commandData.message)
             }
