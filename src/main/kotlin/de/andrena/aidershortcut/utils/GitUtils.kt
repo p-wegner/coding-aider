43c25ce7c75d7cfc95c545f50eca0762e3f21575
Peter Wegner <peter.wegner@andrena.de>
2024-09-04 09:20:15 +0200

    refactor: remove unused imports and add comment for openGitComparisonTool function

--- src/main/kotlin/de/andrena/aidershortcut/utils/GitUtils.kt
+++ src/main/kotlin/de/andrena/aidershortcut/utils/GitUtils.kt
@@ -1,14 +1,13 @@
 package de.andrena.aidershortcut.utils
 
 import com.intellij.openapi.project.Project
-import git4idea.GitUtil
-import git4idea.ui.GitCompareWithRevisionDialog
 
 object GitUtils {
     fun getCurrentCommitHash(project: Project): String? {
         return GitUtil.getCurrentRevision(project)
     }
 
+    // compares the current local file state with the given commit
     fun openGitComparisonTool(project: Project, commitHash: String) {
         GitCompareWithRevisionDialog.show(project, commitHash)
     }
