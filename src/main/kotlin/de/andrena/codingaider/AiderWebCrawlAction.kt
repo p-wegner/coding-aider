66930679805690da11f42a18ad3d913333d51018
Peter <PeterWegner3141@gmail.com>
2024-09-07 01:52:05 +0200

    feat: Add markdown simplification to web crawl action

--- src/main/kotlin/de/andrena/codingaider/AiderWebCrawlAction.kt
+++ src/main/kotlin/de/andrena/codingaider/AiderWebCrawlAction.kt
@@ -8,8 +8,11 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.vladsch.flexmark.html2md.converter.FlexmarkHtmlConverter
+import de.andrena.codingaider.command.CommandData
 import de.andrena.codingaider.command.FileData
+import de.andrena.codingaider.executors.IDEBasedExecutor
 import de.andrena.codingaider.inputdialog.PersistentFileManager
+import de.andrena.codingaider.settings.AiderSettings
 import de.andrena.codingaider.utils.FileRefresher
 import java.io.File
 import java.math.BigInteger
@@ -27,17 +30,6 @@ class AiderWebCrawlAction : AnAction() {
 
             val combinedHash = MessageDigest.getInstance("MD5").digest(url.toByteArray()).let {
                 BigInteger(1, it).toString(16).padStart(32, '0')
-                
-                val commandData = CommandData(
-                    message = "simplify",
-                    useYesFlag = false,
-                    llm = "",
-                    additionalArgs = "",
-                    files = listOf(FileData(filePath, true)),
-                    isShellMode = false,
-                    lintCmd = ""
-                )
-                IDEBasedExecutor(project, commandData).execute()
             }
 
             val pageName = URL(url).path.split("/").lastOrNull() ?: "index"
@@ -51,6 +43,20 @@ class AiderWebCrawlAction : AnAction() {
                 val htmlContent = page.asXml()
                 val markdown = FlexmarkHtmlConverter.builder().build().convert(htmlContent)
                 File(filePath).writeText(markdown)
+                val commandData = CommandData(
+                    message = """
+                        Simplify this markdown file. 
+                        Keep all information that you think will be relevant for code documentation and how to use a certain technology.
+                        If something seems out of context or irrelevant, remove it.
+                    """.trimIndent(),
+                    useYesFlag = false,
+                    llm = AiderSettings.getInstance(project).llm,
+                    additionalArgs = "",
+                    files = listOf(FileData(filePath, true)),
+                    isShellMode = false,
+                    lintCmd = ""
+                )
+                IDEBasedExecutor(project, commandData).execute()
             }
 
             val virtualFile =
