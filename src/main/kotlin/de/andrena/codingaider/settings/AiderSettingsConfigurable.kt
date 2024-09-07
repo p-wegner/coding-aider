dce876457b616b6f63eb69c4b86cd212657e663f
Peter <PeterWegner3141@gmail.com>
2024-09-07 16:41:34 +0200

    feat: Add lint command field to settings UI

--- src/main/kotlin/de/andrena/codingaider/settings/AiderSettingsConfigurable.kt
+++ src/main/kotlin/de/andrena/codingaider/settings/AiderSettingsConfigurable.kt
@@ -80,7 +80,7 @@ class AiderSettingsConfigurable(private val project: Project) : Configurable {
                 row {
                     cell(isShellModeCheckBox)
                 }
-                row {
+                row("Lint Command:") {
                     cell(lintCmdField)
                         .resizableColumn()
                         .align(Align.FILL)
