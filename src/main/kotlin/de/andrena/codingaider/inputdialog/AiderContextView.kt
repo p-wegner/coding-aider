98f0c4d6c4aed5fe584d785d58f34adac6301663
Peter <PeterWegner3141@gmail.com>
2024-09-09 23:00:20 +0200

    organize imports

--- src/main/kotlin/de/andrena/codingaider/inputdialog/AiderContextView.kt
+++ src/main/kotlin/de/andrena/codingaider/inputdialog/AiderContextView.kt
@@ -11,19 +11,19 @@ import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.tree.TreeUtil
 import de.andrena.codingaider.command.FileData
 import java.awt.BorderLayout
-import java.awt.event.KeyEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.io.File
-import javax.swing.AbstractAction
-import javax.swing.JComponent
 import javax.swing.JPanel
-import javax.swing.KeyStroke
 import javax.swing.border.TitledBorder
 import javax.swing.tree.DefaultMutableTreeNode
 import javax.swing.tree.DefaultTreeCellRenderer
 import javax.swing.tree.DefaultTreeModel
 import javax.swing.tree.TreeSelectionModel
+import java.awt.event.KeyEvent
+import javax.swing.AbstractAction
+import javax.swing.JComponent
+import javax.swing.KeyStroke
 import javax.swing.SwingUtilities
 
 class AiderContextView(
