e6441b44a138aa47e04e4871a4f16c64dc3eb63e
Peter Wegner <peter.wegner@andrena.de>
2024-09-12 17:02:31 +0200

    fix import

--- src/main/kotlin/de/andrena/codingaider/executors/ShellExecutor.kt
+++ src/main/kotlin/de/andrena/codingaider/executors/ShellExecutor.kt
@@ -3,6 +3,7 @@ package de.andrena.codingaider.executors
 import com.intellij.openapi.project.Project
 import de.andrena.codingaider.command.AiderCommandBuilder
 import de.andrena.codingaider.command.CommandData
+import de.andrena.codingaider.settings.AiderSettings
 import org.jetbrains.plugins.terminal.TerminalToolWindowManager
 
 class ShellExecutor(
@@ -13,13 +14,15 @@ class ShellExecutor(
         val terminalView = TerminalToolWindowManager.getInstance(project)
         val terminalSession = terminalView.createLocalShellWidget(project.basePath, "Aider", true)
 
-        val command = AiderCommandBuilder.buildAiderCommand(commandData, true, AiderSettings.getInstance(project).useDockerAider).joinToString(" ")
-        
+        val command =
+            AiderCommandBuilder.buildAiderCommand(commandData, true, AiderSettings.getInstance(project).useDockerAider)
+                .joinToString(" ")
+
         if (AiderSettings.getInstance(project).useDockerAider) {
             // Remove DOCKER_HOST if it's set, allowing Docker to use its default configuration
             terminalSession.executeCommand("set DOCKER_HOST=")
         }
-        
+
         terminalSession.executeCommand(command)
     }
 }
