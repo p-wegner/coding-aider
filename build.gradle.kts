1bb17b5c0f1c3fb7e1f00a4c3836dcbcb51ae4f8
Peter <PeterWegner3141@gmail.com>
2024-11-09 18:22:35 +0100

    adjust supported versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,7 +87,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("241.*")
-//        untilBuild.set("242.*")
+        untilBuild.set("243.*")
         version = project.version.toString()
     }
 
