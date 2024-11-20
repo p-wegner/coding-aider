093b2bb1bf11a5bb6b36ac8fa05fa6c4dbc2e806
Peter <PeterWegner3141@gmail.com>
2024-11-20 10:07:16 +0100

    outline methods

--- src/main/kotlin/de/andrena/codingaider/utils/GitUtils.kt
+++ src/main/kotlin/de/andrena/codingaider/utils/GitUtils.kt
@@ -3,13 +3,7 @@ package de.andrena.codingaider.utils
 import com.intellij.openapi.application.ApplicationManager.getApplication
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.changes.Change
-import com.intellij.openapi.vcs.FilePath
-import com.intellij.openapi.vcs.LocalFilePath
-import com.intellij.openapi.vcs.changes.ContentRevision
-import com.intellij.openapi.vcs.changes.CurrentContentRevision
-import com.intellij.openapi.vcs.changes.actions.ShowDiffAction
-import com.intellij.openapi.vcs.history.VcsRevisionNumber
-import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vcs.changes.actions.diff.ShowDiffAction
 import git4idea.GitUtil
 import git4idea.repo.GitRepository
 import java.io.File
@@ -27,19 +21,7 @@ object GitUtils {
             val repository = getGitRepository(project)
             if (repository != null) {
                 getApplication().invokeLater {
-                    val changes = getChanges(repository).map { file ->
-                        val virtualFile = LocalFileSystem.getInstance().findFileByIoFile(file)
-                        if (virtualFile != null) {
-                            val filePath = LocalFilePath(virtualFile.path, virtualFile.isDirectory)
-                            val beforeRevision = object : ContentRevision {
-                                override fun getContent() = ""
-                                override fun getFile() = filePath
-                                override fun getRevisionNumber() = VcsRevisionNumber.NULL
-                            }
-                            val afterRevision = CurrentContentRevision(filePath)
-                            Change(beforeRevision as ContentRevision, afterRevision)
-                        } else null
-                    }.filterNotNull()
+                    val changes = getChangesSince(repository,commitHash)
 
                     ShowDiffAction.showDiffForChange(
                         project,
@@ -51,11 +33,9 @@ object GitUtils {
         }
     }
 
-    private fun getChanges(repository: GitRepository): List<File> {
-        val root = repository.root
-        return repository.untrackedFilesHolder.retrieveUntrackedFilePaths().map { filePath ->
-            File(root.path, filePath.path)
-        }
+    private fun getChangesSince(repository: GitRepository, commitHash: String): List<Change> {
+        // TODO: implement this
+        throw NotImplementedError()
     }
 
     private fun getGitRepository(project: Project): GitRepository? {
