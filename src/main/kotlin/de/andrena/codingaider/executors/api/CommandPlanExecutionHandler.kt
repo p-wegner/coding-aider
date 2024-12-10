3a4bd17c3c871db56c91c454b138c837f2cae382
Peter Wegner <peter.wegner@andrena.de>
2024-12-10 12:12:22 +0100

    feat: Add TODOs for setting active plan in CommandPlanExecutionHandler

--- src/main/kotlin/de/andrena/codingaider/executors/api/CommandPlanExecutionHandler.kt
+++ src/main/kotlin/de/andrena/codingaider/executors/api/CommandPlanExecutionHandler.kt
@@ -24,10 +24,13 @@ class CommandPlanExecutionHandler(private val project: Project, private val comm
                 project.service<PersistentFileService>().addFile(FileData(file.absolutePath, false))
             }
         }
+        // TODO: Set Active Plan when plan file is created in plan folder
 
     }
 
     fun beforeCommandStarted() {
+        // TODO: Set Active Plan depending on plan file in plan folder
+
         val plansFolder = File(project.basePath, AiderPlanService.AIDER_PLANS_FOLDER)
         if (plansFolder.exists() && plansFolder.isDirectory) {
             initialPlanFiles = plansFolder.listFiles { file -> file.isFile && file.extension == "md" }
