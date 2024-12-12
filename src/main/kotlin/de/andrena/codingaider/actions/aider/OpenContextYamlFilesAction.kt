ba3cfee1ba1d82fe33420a21611d5c106cd14953
Peter Wegner <peter.wegner@andrena.de>
2024-12-12 16:18:31 +0100

    fix thread for action

--- src/main/kotlin/de/andrena/codingaider/actions/aider/OpenContextYamlFilesAction.kt
+++ src/main/kotlin/de/andrena/codingaider/actions/aider/OpenContextYamlFilesAction.kt
@@ -1,5 +1,6 @@
 package de.andrena.codingaider.actions.aider
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -10,24 +11,26 @@ import de.andrena.codingaider.model.ContextFileHandler
 import java.io.File
 
 class OpenContextYamlFilesAction : AnAction() {
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+    
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         val file = e.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
-        
+
         if (!file.name.endsWith(".context.yaml")) {
             return
         }
 
         val contextFile = File(file.path)
         val projectBasePath = project.basePath ?: return
-        
+
         val files = ContextFileHandler.readContextFile(contextFile, projectBasePath)
         openFiles(files.map { it.filePath }, project)
     }
 
     private fun openFiles(filePaths: List<String>, project: Project) {
         val fileEditorManager = FileEditorManager.getInstance(project)
-        
+
         filePaths.forEach { path ->
             val file = LocalFileSystem.getInstance().findFileByPath(path)
             file?.let { fileEditorManager.openFile(it, true) }
