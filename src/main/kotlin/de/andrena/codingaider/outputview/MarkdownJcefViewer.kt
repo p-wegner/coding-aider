366334e4d7e67e31d918695ee151f2ca43269984
Peter <PeterWegner3141@gmail.com>
2025-04-27 17:18:27 +0200

    fix: add TODO comment for potential null browser issue in MarkdownJcefViewer

--- src/main/kotlin/de/andrena/codingaider/outputview/MarkdownJcefViewer.kt
+++ src/main/kotlin/de/andrena/codingaider/outputview/MarkdownJcefViewer.kt
@@ -157,6 +157,7 @@ class MarkdownJcefViewer(private val lookupPaths: List<String> = emptyList()) {
                     switchToFallbackEditor()
                 }
                 override fun onLoadingStateChange(browser: CefBrowser?, isLoading: Boolean, canGoBack: Boolean, canGoForward: Boolean) {}
+                // TODO 27.04.2025 pwegner: this seems to be null!
             }, jbCefBrowser!!.cefBrowser) // Pass null to handle all frames, or jbCefBrowser!!.cefBrowser for main frame only
         }
 
