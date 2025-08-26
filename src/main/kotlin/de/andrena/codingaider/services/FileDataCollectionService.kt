75aaea12af3bea02d90ad36f2c04ae5cdee444df
Peter <PeterWegner3141@gmail.com>
2025-08-26 19:30:24 +0200

    refactor: Improve file collection service with minor cleanup and TODO comment

--- src/main/kotlin/de/andrena/codingaider/services/FileDataCollectionService.kt
+++ src/main/kotlin/de/andrena/codingaider/services/FileDataCollectionService.kt
@@ -10,6 +10,7 @@ import de.andrena.codingaider.settings.AiderSettings
 import de.andrena.codingaider.settings.AiderSettings.Companion.getInstance
 import de.andrena.codingaider.utils.FileTraversal
 import de.andrena.codingaider.utils.ScratchFileUtils
+import fleet.tracing.meter
 
 @Service(Service.Level.PROJECT)
 class FileDataCollectionService(private val project: Project) {
@@ -17,7 +18,6 @@ class FileDataCollectionService(private val project: Project) {
     private val settings: AiderSettings = getInstance()
 
     private val aiderIgnoreService = project.service<AiderIgnoreService>()
-
     fun collectAllFiles(
         files: Array<VirtualFile> = emptyArray(),
         includePersistentFiles: Boolean = true
@@ -39,7 +39,7 @@ class FileDataCollectionService(private val project: Project) {
         if (this.settings.enableDocumentationLookup) {
             traversedFiles.addAll(project.service<DocumentationFinderService>().findDocumentationFiles(files))
         }
-
+        // TODO 26.08.2025 pwegner: expand context yaml files if setting is on
         return traversedFiles.distinctBy { normalizePath(it.filePath) }
     }
 
